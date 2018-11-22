3859cc3bcaa51cf21c08f624cbb4862a1fa1cb19
unknown <xy14198725@163.com>
2018-11-23 00:37:39 +0800

    上传图像

--- src/pages/page-demo/index.vue
+++ src/pages/page-demo/index.vue
@@ -1,7 +1,11 @@
 <template>
     <d2-container better-scroll>
         <template slot="header">header</template>
-         hello world
+        <el-upload class="upload-demo" drag action="https://jsonplaceholder.typicode.com/posts/" multiple>
+            <i class="el-icon-upload"></i>
+            <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+            <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+        </el-upload>
         <template slot="footer">footer</template>
     </d2-container>
 </template>
