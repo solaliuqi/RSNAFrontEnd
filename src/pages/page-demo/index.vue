403bb9533ed85f3be338cd102b42df8648819831
solaliuqi <1341220428@qq.com>
2018-11-22 20:04:58 +0800

    first1 commit

---
+++ src/pages/page-demo/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <d2-container better-scroll>
+        <template slot="header">header</template>
+         hello world
+        <template slot="footer">footer</template>
+    </d2-container>
+</template>
+
+<!--<script>-->
+  <!--export default {-->
+    <!--name: 'index.vue'-->
+  <!--}-->
+<!--</script>-->
+
+<style scoped>
+
+</style>
