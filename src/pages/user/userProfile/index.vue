a7c9450c14ab40d05753bd3982c0ff4027548c98
Xiaying <xy14198725@163.com>
2019-01-15 20:44:35 +0800

    用户头像控件

--- src/pages/user/userProfile/index.vue
+++ src/pages/user/userProfile/index.vue
@@ -39,11 +39,22 @@
                                        v-model="selected"></area-cascader>
                     </el-form-item>
                     <el-form-item prop="address">
-                        <el-input placeholder="具体地址" v-model="userForm.address" style="width:600px"></el-input>
+                        <el-input placeholder="具体地址" v-model="userForm.address" style="width:500px"></el-input>
                     </el-form-item>
                 </el-form>
             </div>
-            <div class="">
+            <div class="user-profile-right">
+                <el-upload
+                        class="avatar-uploader"
+                        action="123"
+                        :show-file-list="false"
+                        :on-success="handleAvatarSuccess"
+                        :on-change="onchange"
+                        :before-upload="beforeAvatarUpload">
+                <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                </el-upload>
+
 
             </div>
         </div>
@@ -76,13 +87,42 @@
                 placeholder: [" ", " ", " "],
                 selected: [],
 
-                rules: {}
+                rules: {},
+
+                imageUrl: ''
             }
         },
         methods: {
             ...mapGetters('d2admin/user', [
                 'getUserInfo'
-            ])
+            ]),
+          handleAvatarSuccess(res, file) {
+            this.imageUrl = URL.createObjectURL(file.raw);
+          },
+          beforeAvatarUpload(file) {
+            const isJPG = file.type === 'image/jpeg';
+            const isLt2M = file.size / 1024 / 1024 < 2;
+
+            if (!isJPG) {
+              this.$message.error('上传头像图片只能是 JPG 格式!');
+            }
+            if (!isLt2M) {
+              this.$message.error('上传头像图片大小不能超过 2MB!');
+            }
+            return isJPG && isLt2M;
+          },
+            //当上传图片后，调用onchange方法，获取图片本地路径
+          // onchange(file, fileList){
+          //   var _this = this;
+          //   var event = event || window.event;
+          //   var file = event.target.files[0];
+          //   var reader = new FileReader()
+          //   //转base64
+          //   reader.onload = function(e){
+          //     _this.imageUrl = e.target.result // 将图片路径赋值给src
+          //   }
+          //   reader.readAsDataURL(file);
+          // },
         },
         created: function () {
             this.userForm.username = this.getUserInfo().name
@@ -91,3 +131,85 @@
         }
     }
 </script>
+<style>
+    .user-profile {
+        width: 100%;
+        margin-left: 10px;
+        background: white;
+        padding: 30px;
+
+    .user-profile-left {
+        width: 50%;
+        float: left;
+        margin-right: 20px;
+    }
+
+    .user-profile-right {
+        width: 40%;
+        float: left;
+        height: 100px;
+    /*.avatar-uploader .el-upload {*/
+        /*border: 1px dashed #d9d9d9;*/
+        /*border-radius: 6px;*/
+        /*cursor: pointer;*/
+        /*position: relative;*/
+        /*overflow: hidden;*/
+    /*}*/
+
+    /*.avatar-uploader .el-upload:hover {*/
+        /*border-color: #409EFF;*/
+    /*}*/
+
+    /*.avatar-uploader-icon {*/
+        /*font-size: 28px;*/
+        /*color: #8c939d;*/
+        /*width: 178px;*/
+        /*height: 178px;*/
+        /*line-height: 178px;*/
+        /*text-align: center;*/
+    /*}*/
+
+    /*.avatar {*/
+        /*width: 178px;*/
+        /*height: 178px;*/
+        /*display: block;*/
+    /*}*/
+
+
+
+    }
+
+    }
+
+    .avatar-uploader .el-upload {
+        border: 1px dashed #d9d9d9;
+        border-radius: 6px;
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+    }
+
+    .avatar-uploader .el-upload:hover {
+        border-color: #409EFF;
+    }
+
+    .avatar-uploader-icon {
+        font-size: 28px;
+        color: #8c939d;
+        width: 178px;
+        height: 178px;
+        line-height: 178px;
+        text-align: center;
+    }
+
+    .avatar {
+        width: 178px;
+        height: 178px;
+        display: block;
+    }
+
+
+
+
+
+</style>
